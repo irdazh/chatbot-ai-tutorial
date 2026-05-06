8b207046f2977848944537bbfd06f7fe9a50f27b
irdazh <=>
2026-05-06 08:54:25 +0700

    create the streamlit app for the ui

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 ## Chatbot AI
-From youtube tutorial
+
+Using OpeanAI API from this [youtube tutorial.](https://www.youtube.com/watch?v=q5HiD5PNuck)
+Nah, I decided to just follow one from Gemini, and scrape things from the tutorial. Well, what can I do? 
+
+Oh. And another btw, I use groq model. Using FastAPI as the backend and streamlit for the UI yeay. Bye.
+
+
+## Tutorial steps
+1. Installation
+2. Creating Files
+2. Ask Gemini and Copy Pasting Code
+3. Done, hehe. 
+
+### Setting things up
+```
+1. py -3.11 -m venv .venv
+2. source .venv/Scripts/activate #or .venv/bin/activate
+3. python -m pip install python-dotenv groq google-generativeai ....
+```
+
+### Free option you have.
+1. Groq
+2. Google Gemini
+
+### Well, here is steps by 
+1. 
+
---
+++ ui1.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import requests
+import time
+
+st.set_page_config(page_title = "Groq Photography Chatbot", page_icon=":camera:", layout="wide")
+st.title("Groq Photography Chatbot")
+
+
+# Initialize chat history
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat history
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+# User Input
+if prompt := st.chat_input("How can I help you today? Ask me anything about photography! To exit, just say 'bye'."):
+
+    # If input including bye, exit the chat
+    if prompt.lower() in ["exit", "quit", 'bye']:
+        st.write("Exiting chat. Goodbye!")
+        st.session_state.messages = []  # Clear chat history
+        with st.spinner("Clearing chat..."): # Animate and delay for better UX
+            time.sleep(2) 
+        st.rerun()  # Refresh the app to clear the UI
+    
+    # Add user message to UI
+    st.session_state.messages.append(
+        {"role": "user", "content":prompt}
+    )
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Call the backend API 
+    with st.chat_message("assistant"):
+        try:
+            # Point to localhost for now
+            res = requests.post("http://localhost:8000/chat", json={"message": prompt})
+            answer = res.json().get("response")
+            st.markdown(answer)
+
+            # Add assistant message to UI
+            st.session_state.messages.append(
+                {"role": "assistant", "content": answer}
+            )
+
+        except Exception as e:
+            st.error(f"Failed to connect to the brain! Error: {e}")
+    
